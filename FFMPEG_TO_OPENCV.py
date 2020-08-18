c072af0943d5b204b33b38cde87f929a74ce18eb
ND <ndamen3@users.noreply.github.com>
2020-08-18 17:10:48 -0400

    Update FFMPEG_TO_OPENCV.py
    Latency is 4 seconds from phone to python window

--- FFMPEG_TO_OPENCV.py
+++ FFMPEG_TO_OPENCV.py
@@ -21,10 +21,10 @@ while True:
 	raw_image = pipe.stdout.read(IMG_W*IMG_H*3)
 	image =  numpy.frombuffer(raw_image, dtype='uint8')		# convert read bytes to np
 	image = image.reshape((IMG_H,IMG_W,3))
-	frame2 = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-	ret,frame3 = cv2.threshold(frame2,80,255,cv2.THRESH_BINARY)
-	fgmask4 = fgbg4.apply(frame3)
-	roughOutput = cv2.bitwise_and(image, image, mask=fgmask4)
+	#frame2 = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+	#ret,frame3 = cv2.threshold(frame2,80,255,cv2.THRESH_BINARY)
+	#fgmask4 = fgbg4.apply(frame3)
+	#roughOutput = cv2.bitwise_and(image, image, mask=fgmask4)
 
 	cv2.imshow('Video', image)
 	#cv2.imshow('Video2', frame2)
