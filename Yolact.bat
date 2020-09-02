10ed45e0f562e57db0d6ecc18074be05b6ef0db4
ND <ndamen3@users.noreply.github.com>
2020-09-02 13:25:25 -0400

    Changed Stream ID in BAT

--- Yolact.bat
+++ Yolact.bat
@@ -1,4 +1,4 @@
 call C:\ProgramData\Anaconda3\Scripts\activate.bat
 call conda activate yolact
 call cd C:\Users\nated\Documents\Python Scripts\Cloned Repo\yolact
-call python eval.py --trained_model=weights/yolact_resnet50_54_800000.pth --score_threshold=0.15 --top_k=15 --video_multiframe=18 --display_scores=False --display_text=False --greenscreen=True --video="rtmp://192.168.1.202/live/test"
+call python eval.py --trained_model=weights/yolact_resnet50_54_800000.pth --score_threshold=0.15 --top_k=15 --video_multiframe=18 --display_scores=False --display_text=False --greenscreen=True --video="rtmp://192.168.1.193/live/test"
